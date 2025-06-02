20b71dfc75af0c601223fef362be117132752c65
KaylaChika12 <kayla.122140009@student.itera.ac.id>
2025-06-02 09:54:05 +0700

    Delete src directory

--- src/api/auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_BASE = 'http://localhost:6543/api'; // 
-
-export const loginUser = (username, password) =>
-  axios.post(`${API_BASE}/login`, { username, password });
-
-export const registerUser = (username, email, password) =>
-  axios.post(`${API_BASE}/register`, { username, email, password });
